67ce32050a3fa5d27ac656dddaede96e515a3967
DwitaMeiriska02 <dwitameiriska02@gmail.com>
2023-04-04 19:02:29 +0700

    first commit

---
+++ resources/views/Tampilan.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <title>Bumi Rafflesia</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+            <div class="logo"><a href=''>Bumi Rafflesia</a></div>
+            <div class="menu">
+                <ul> 
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#kuliner">Kuliner</a></li>
+                    <li><a href="#ekonomi">Ekonomi</a></li> <li><a href="#destinasi">Destination</a></li>
+
+                    <li><a href="#contact">contact</a></li>
+                    <li><a href="sign.html" class="tbl-biru ">sign in</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="wrapper">
+        <!----------untuk home-->
+        <section id="home"> 
+            <img src="https://media-cdn.tripadvisor.com/media/photo-s/12/7a/b3/23/pasir-putih-resort.jpg"/>
+            <div class="kolom">
+                <p clas="deskripsi">Peninapan Di Bengkulu </p>
+                <h2>Hotel Pasir Putih</h2>
+                <p> Ketika kalian berkunjung ke Kota bengkulu kami sarankan penginapan terdekat dengan lokasi pantai panjang Bengkulu tepatnya daerah,Jl. Pariwisata Pantai Panjang No.11, Lempuing, Kec. Ratu Agung, Kota Bengkulu, Bengkulu </p>
+                <fermentasi class=""><a href="penginapan.html" class="tbl-green">Lihat Lebih lanjut</a>
+            </div>
+        </section>
+        <!-------untuk kuliner--> 
+        <section id="kuliner">
+            <div class="kolom">
+                <p class="deskripsi">Makanan yang meski kalian coba jika berkunjung ke Bengkulu</p>
+                <h2>Kuliner Khas Bengkulu </h2>
+                <p></p>
+                <p>Mungkin diantara kita sudah tidak asing lagi dengan makanan khas bengkulu yang satu ini, akrab di kenal dengan Tempoyak. Tempoyak adalah salah satu bumbu makanan yang terdapat banyak di Sumatera, tempoyak juga menjadi popular di Bengkulu karena bisa dicampur dan diolah dengan udang. Kuliner ini adalah jenis makanan yang unik. Bagaimana tidak, bahan yang digunakan untuk membuatnya harus difermentasi terlebih dahulu. Adapun bahan tersebut adalah buah durian yang kemudian difermentasi sehingga menciptakan cita rasa yang asam akibat proses </p>
+                <fermentasi class=""><a href="more.html" class="tbl-green">Lihat Lebih lanjut</a>
+            </div>
+            <img src="https://4.bp.blogspot.com/-NOxkF76jcQg/VrgPPmoOAYI/AAAAAAAADpg/e4r266O4oRw/s1600/Resep%2BSambal%2BTempoyak%2BKhas%2BBengkulu.jpg"/>
+        </section>
+        <!-------untuk ekonomi-->
+        <section id="ekonomi">
+            <div class="tengah">
+                <div class="kolom">
+                    <p class="deskripsi">Mata Pencaharian</p>
+                    <h2>Ekonomi</h2>
+                    <p>Seperti diketahui, Bengkulu merupakan provinsi ketiga penghasil kopi terbesar di Indonesia setelah Sumatera Selatan dan Lampung. Bengkulu yang masuk “Segitiga Emas Robusta” di pulau Sumatera ini, setidaknya beberapa tahun terakhir sudah mulai diperhitungkan keberadaannya,selain kopi wilayah bengkulu juga biasanya berkerja sebahagi nelayan ,dan petani  </p>
+                </div>
+
+                <div class="list-pencaharian">
+                    <div class="jenis">
+                        <img src="https://asset.kompas.com/crops/hdXkK75zv7EbvEsxv6AS_JJriN0=/0x244:900x844/750x500/data/photo/2022/01/19/61e778df1e7a0.jpg"/>
+                        <p>Berkebun Kopi</p>
+                    </div>
+                    <div class="jenis">
+                        <img src="https://cdn1-production-images-kly.akamaized.net/jZbOAWNBxzbemEj1du_lAncy-gI=/1231x710/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3227179/original/015505900_1599109672-20200903-Nelayan-Palestina-1.jpg"/>
+                        <p>Nelayan</p>
+                    </div>
+                    <div class="jenis">
+                        <img src="https://static.republika.co.id/uploads/images/inpicture_slide/pekerja-memanen-kopi-varietas-robusta-di-desa-_150309074854-690.jpg"/>
+                        <p>Bertani Padi</p>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+        <!--------untuk destinasi-->
+        <section id="destinasi">
+            <div class="tengah">
+                <div class="kolom">
+                    <p class="deskripsi">wisata yang harus kamu kunjungi</p>
+                    <h2>Destinatiaon</h2>
+                    <p>Provinsi bengkulu mempunyai banyak wisata yang bisa kalian kunjungi,seperti pantai panjang , Musieum soekarno yang merupakan tempat tinggal dari Ir.Soekarno,serta terdapat benteng Marlborough benteng peninggalan inggis. </p>
+                </div>
+
+                <div class="list-wisata">
+                    <div class="nama-wisata">
+                        <img src="https://indonesiatraveler.id/wp-content/uploads/2020/09/Bengkulu-Benteng-Marlborough1-e1599580790147.jpg"/>
+                        <p>Benteng Marlborough.</p>
+                    </div>
+                    <div class="nama-wisata">
+                         <img src="https://awsimages.detik.net.id/community/media/visual/2020/07/04/rumah-pengasingan-sukarno-bung-karno-di-bengkulu-dok-istimewa_169.jpeg?w=700&q=90"/>
+                         <p>Museum Rumah Bung Karno</p>
+                    </div>
+                    <div class="nama-wisata">
+                         <img src="https://cdn.nativeindonesia.com/foto/pantai-panjang/Landmark-Pantai-Panjang.jpg"/>
+                         <p>Pantai Panjang</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <div id="contact">
+        <div class ="wrapper">
+            <div class="footer">
+                <div class="footer-section">
+                    <h3>Bumi Rafflesia</h3>
+                    <p> Bumi Rafflesia terletak Provinsi Bengkulu, Kota Bengkulu,Kecamatan Teluk Segara, & Kecamatan Ratu Samban.</p>
+                </div>
+                 <div class="footer-section">
+                    <h3>Contact Person</h3>
+                    <p>Anda dapat menghubungi kami melalui media berikut ini!!</p>
+                    <p>telp: 0878 3341 2299: a.n Riska</p>
+                    <p>Wa: 0853 819 2019: a.n Cinta</p>
+                </div>
+                <div class="footer-section">
+                    <h3>Social Media</h3>
+                    <p><b>YouTube:</b>Bumi Raflesia </p>
+                    <p><b>Instagram:</b>@Bumi_Raflesia_Bengkulu </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="Copyright">
+        <div class="wrapper">
+            &copy; 2023 <b>By Dwita meiriska</b> All Rights Reserved
+
+        </div>
+
+    </div>
+</body>
+</html>
